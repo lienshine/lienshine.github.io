39d42619ceeb7592e3c47e09116da8b5011d3035
SongXinXin <1368084801@qq.com>
2023-01-13 15:27:22 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,20 +2,8 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 
 export default {
+    base:"/",
     theme: defaultTheme({
-      // 在这里进行配置
+      
     }),
-    markdown: {
-        // the markdown-it-anchor
-            anchor: { permalink: false },
-        // the markdown-it-toc extension
-            toc: { includeLevel: [1, 2] },
-        config: md => {
-            d.use(require('markdown-it')({
-                breaks:true
-              }))
-            // this is where you add more markdown-it extensions
-            md.use(require('markdown-it-name-of-extension'))
-            }
-        }
   }
